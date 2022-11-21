231b87e7fa5606b40ef74f4b19c191d2f337987a
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-11-20 22:34:44 -0800

    complete expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+A: Manually run them locally before pushing code. It is because it make convenient and easy for other people to work with the same program and make sure the code that would be pushed works correctly.
+
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+A: No, because unit test is more suitable for such testing while e2e is more adequate for the whole page.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+A: No I would use end to end testing because "message" feature is more complicated thing to test,unit test is basically designed for something simpler.
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+A: Yes, it is because unit test is adequate for testing such a function that checking the length of a given message.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,37 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('isPhoneNumber', () => {
+    expect(functions.isPhoneNumber("619-494-6775")).toBe(true);
+    expect(functions.isPhoneNumber("+1(619)494-6775")).toBe(true);
+    expect(functions.isPhoneNumber("123456789")).toBe(false);
+    expect(functions.isPhoneNumber("abc1234567")).toBe(false);
+});
+
+test('isEmail', () => {
+    expect(functions.isEmail("scottie@gmail.com")).toBe(true);
+    expect(functions.isEmail("asdf@ucsd.edu")).toBe(true);
+    expect(functions.isEmail("notaemail")).toBe(false);
+    expect(functions.isEmail("notamail.com")).toBe(false);
+});
+
+test('isStrongPassword', () => {
+    expect(functions.isStrongPassword("StrongPassword")).toBe(true);
+    expect(functions.isStrongPassword("scottieboy05")).toBe(true);
+    expect(functions.isStrongPassword("111")).toBe(false);
+    expect(functions.isStrongPassword("1zadbsh")).toBe(false);
+});
+
+test('isDate', () => {
+    expect(functions.isDate("10/11/1999")).toBe(true);
+    expect(functions.isDate("1/1/2023")).toBe(true);
+    expect(functions.isDate("2023/1/1")).toBe(false);
+    expect(functions.isDate("01/01/23")).toBe(false);
+});
+
+test('isHexColor', () => {
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+    expect(functions.isHexColor("#000000")).toBe(true);
+    expect(functions.isHexColor("#GGGGGG")).toBe(false);
+    expect(functions.isHexColor("#FFFFFG")).toBe(false);
+});
